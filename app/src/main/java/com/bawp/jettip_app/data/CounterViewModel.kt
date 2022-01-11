db4672c799d8758acb877aa003908866f532cc5c
viradhanus <e16083@eng.pdn.ac.lk>
2022-01-11 12:05:32 +0530

    Initial commit

---
+++ app/src/main/java/com/bawp/jettip_app/data/CounterViewModel.kt
@@ -0,0 +1,22 @@
+package com.bawp.jettip_app.data
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel: ViewModel() {
+   private val counter: MutableState<Int> = mutableStateOf(0)
+
+    fun increaseCounter() {
+        counter.value = counter.value + 1
+    }
+    fun decreaseCounter() {
+        if (counter.value > 0)
+           counter.value = counter.value - 1
+    }
+    fun getCounter(): Int {
+       return counter.value
+    }
+
+
+}
